b90d302b0c9c66bee187ccfbd44161fb14737ecc
Mikovhe <mjmukwevho@outlook.com>
2021-09-14 20:03:00 +0200

    model &streamlit

--- deploy/main.py
+++ deploy/main.py
@@ -4,7 +4,16 @@ import streamlit as st
 def main():
     """function that puts together the streamlit app"""
 
-    print("I am the best in the world")
+    page = st.sidebar.selectbox("Choose a page",["About app","Data Explorer","Machine Learning"])
 
+    if page =="About app":
+        st.title("The Last Mile Training Streamlit App")
+
+        st.markdown(""" The purpose of this app is to showcase some streamlit usecases. We have a movie rating 
+        system built and have to deploy it. Go Crazy!
+        """)
+
+    if page =="Data Explorer":
+        st.title("build a mini data explorer here and show some EDA!")
 if __name__== "__main__":
     main()
